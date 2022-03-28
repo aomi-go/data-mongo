5dd4ca2532eec7a6c531f96ca3673823b2c2d64d
尼古拉斯丶少 <sean.snow@live.com>
2022-03-28 14:01:48 +0800

    fix: 去掉泛型

--- repository/PagingAndSortingRepository.go
+++ repository/PagingAndSortingRepository.go
@@ -5,6 +5,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type PagingAndSortingRepository[T interface{}, ID interface{}] interface {
-	FindAll(filter interface{}, pageable *datacommon.Pageable, opts ...*options.FindOptions) datacommon.Page[T]
+type PagingAndSortingRepository interface {
+	FindAll(filter interface{}, pageable datacommon.Pageable, opts ...*options.FindOptions) datacommon.Page
 }
